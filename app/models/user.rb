df278600536da7ba00d824ccf8268fe9a74173d9
Tadesse Jemal <tadessejemal63@gmail.com>
2023-03-03 16:47:49 +0300

    add user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
 class User < ApplicationRecord
     has_secure_password
+    # username validations
+    validates_presence_of :username
+    validates_uniqueness_of :username, :case_sensitive => false
+    validates :username, :length => { :in => 6..30}
+    # password validations
+    PASSWORD_FORMAT = /\A
+        (?=.*[A-Z]) # Must contain an uppercase character
+        (?=.*[a-z]) # Must contain a lowercase character
+        (?=.*\d) # Must contain a digit
+        (?=.*[[:^alnum:]]) # Must contain a symbol
+    /x
+    has_secure_password
+    validates :password, :length => { :minimum => 6 }
+    validates :password, format: { with: PASSWORD_FORMAT, :message => 'Password must include: 1 uppercase, 1 lowercase, 1 digit and 1 special 
+         character' }
 end
